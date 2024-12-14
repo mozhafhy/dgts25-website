7a93e6a0ad4451e04f46fa9ac422c5c8c04169c7
kaila asus <mochzhafif1123@student.ub.ac.id>
2024-12-14 20:48:32 +0700

    feat: added components

--- dgts-website/src/App.jsx
+++ dgts-website/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Navbar from "./components/navbar/Navbar.jsx";
+import Hero from "./components/hero/Hero.jsx";
+import Sponsor from "./components/sponsor/Sponsor.jsx";
+import About from "./components/about/About.jsx";
+import ImgCarousel from "./components/img-carousel/ImgCarousel.jsx";
+import Events from "./components/events-date/Events.jsx";
+import Inti from "./components/kepanitiaan/inti/Inti.jsx";
+import Divisi from "./components/kepanitiaan/divisi/Divisi.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Hero />
+      <Sponsor />
+      <About />
+      <ImgCarousel />
+      <Events />
+      <Inti />
+      <Divisi />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
