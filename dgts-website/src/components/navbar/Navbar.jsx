4f062e8ef8f1b459aad704dd3eaf31009316b6ba
kaila asus <mochzhafif1123@student.ub.ac.id>
2024-12-23 15:53:40 +0700

    feat: static version of the navbar finished

--- dgts-website/src/components/navbar/Navbar.jsx
+++ dgts-website/src/components/navbar/Navbar.jsx
@@ -1,7 +1,34 @@
+import "./Navbar.css";
+import Logo from "../../assets/react.svg";
+import Drop from "../../assets/dropdown.svg";
+
 export default function Navbar() {
-  return(
+  return (
     <>
-    
+      <nav className="navbar">
+        <img className="logo" src={Logo} />
+        <ul className="opsi">
+          <li className="dropdown-presensi">
+            <u id="label">Presensi</u>
+            <img src={Drop} />
+          </li>
+          <li>
+            <a className="link" href="/">
+              Tiket
+            </a>
+          </li>
+          <li>
+            <a className="link" href="/">
+              Kontak
+            </a>
+          </li>
+          <li>
+            <a className="link" href="/">
+              Mapping
+            </a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
