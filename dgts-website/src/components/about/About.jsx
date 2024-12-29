fcf7bd2fa2335a1a3420ab2c8b13210cb14cf7ee
HimawanRakha <himawanrakha11@gmail.com>
2024-12-29 14:43:34 +0700

    events4

--- dgts-website/src/components/about/About.jsx
+++ dgts-website/src/components/about/About.jsx
@@ -8,17 +8,15 @@ const dummy = (
 
 export default function About() {
   return (
-    <div className="profile-container">
+    <div className="profile-container shadow-lg">
       <div className="about-container">
         <div className="about">
           <h1 id="title" className="abt">
             About DGTS 2025
           </h1>
           <p id="desc">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
           </p>
         </div>
 
@@ -27,10 +25,8 @@ export default function About() {
             Why DGTS 2025?
           </h6>
           <p id="desc">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
           </p>
         </div>
       </div>
