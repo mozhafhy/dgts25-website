a3d367c388fe8ed8448edccfa5ea2d9c46ec2ec6
HimawanRakha <himawanrakha11@gmail.com>
2024-12-29 21:59:46 +0700

    footerupdate

--- dgts-website/src/components/footer/Footer.jsx
+++ dgts-website/src/components/footer/Footer.jsx
@@ -1,7 +1,40 @@
+import "./Footer.css";
+
 export default function Footer() {
-  return(
-    <>
-    
-    </>
+  return (
+    <footer>
+      <div className="container">
+        <div className="row h-100">
+          <div className="col d-flex justify-content-center align-items-center ">
+            <div className="row">
+              <p className="h5">Social Media</p>{" "}
+            </div>
+
+            <div className="row">
+              <i className="bi bi-linkedin"></i>
+              <i className="bi bi-instagram"></i>{" "}
+            </div>
+          </div>
+          <div className="col d-flex justify-content-center align-items-center ">
+            <div className="row">
+              <p className="h5">Social Media</p>
+            </div>
+            <div className="row">
+              <i className="bi bi-linkedin"></i>
+              <i className="bi bi-instagram"></i>
+            </div>
+          </div>
+          <div className="col d-flex justify-content-center align-items-center ">
+            <div className="row">
+              <p className="h5">Social Media</p>
+            </div>
+            <div className="row">
+              <i className="bi bi-linkedin"></i>
+              <i className="bi bi-instagram"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
