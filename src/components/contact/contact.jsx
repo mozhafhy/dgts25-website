e7e26feafb1af71cc58721be3da3cd04b70876fb
kaila asus <mochzhafif1123@student.ub.ac.id>
2025-01-08 11:19:17 +0700

    added contact button

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,27 +1,18 @@
 import "./contact.css";
+import telephone from "../../assets/telephone.svg";
 
 export default function ContactButton() {
   return (
-    <div className="kontak">
-      <a
-        href="https://wa.me/6282247433645"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-telephone-fill"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill="black"
-            d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
-          />
-        </svg>
-      </a>
-    </div>
+    <a
+      href="https://wa.me/6282247433645"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="kontak-container">
+        <div className="float-button">
+          <img src={telephone} />
+        </div>
+      </div>
+    </a>
   );
 }
