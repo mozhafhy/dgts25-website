46278aef2afc984a427e8c6cc6fd75ff5837a0df
kaila asus <mochzhafif1123@student.ub.ac.id>
2025-01-08 20:56:18 +0700

    added click for to show more images in divisi, TODO: display staff's surname

--- src/components/6kepanitiaan/2divisi/Divisi.jsx
+++ src/components/6kepanitiaan/2divisi/Divisi.jsx
@@ -1,6 +1,6 @@
-/* eslint-disable react/prop-types */
-import "./Divisi.css";
+import "./Divisi.css"
 import dataDivisi from "./Divisi.json";
+import Sie from "./sie/Sie.jsx"
 
 export default function Divisi() {
   return (
@@ -12,52 +12,11 @@ export default function Divisi() {
           isOddChild={divisi.isOddChild}
           divisi={divisi.divisi}
           fotoCO={divisi.fotoCO}
+          namaCO={divisi.namaCO}
           deskripsi={divisi.deskripsi}
           foto={divisi.foto}
         />
       ))}
-    </div>
-  );
-}
-
-function Sie(props) {
-  const fotos = props.foto;
-  const length = fotos.length;
-  const remain = length - 4;
-
-  return props.isOddChild ? (
-    <div key={props.id} id="divisi">
-      <div id="info">
-        <div>
-          <p className="nama-divisi">{props.divisi}</p>
-          <p className="deskripsi-divisi">{props.deskripsi}</p>
-        </div>
-        <div className="pp-anggota">
-          {fotos.slice(0, 4).map((foto, key) => (
-            <img id="pp" src={foto} key={key} alt={`Staff-${key}`} />
-          ))}
-
-          {remain > 0 ? <div id="remain">+{remain}</div> : ""}
-        </div>
-      </div>
-      <img id="foto-co" src={props.fotoCO} />
-    </div>
-  ) : (
-    <div key={props.id} id="divisi">
-      <img id="foto-co" src={props.fotoCO} />
-      <div id="info">
-        <div>
-          <p className="nama-divisi">{props.divisi}</p>
-          <p className="deskripsi-divisi">{props.deskripsi}</p>
-        </div>
-        <div className="pp-anggota">
-          {fotos.slice(0, 4).map((foto, key) => (
-            <img id="pp" src={foto} key={key} alt={`Staff-${key}`} />
-          ))}
-
-          {remain > 0 ? <div id="remain">+{remain}</div> : ""}
-        </div>
-      </div>
     </div>
   );
 }
---
+++ src/components/6kepanitiaan/2divisi/sie/Sie.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/prop-types */
+import "./Sie.css";
+import { useState } from "react";
+
+export default function Sie(props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  const fotos = props.foto;
+  const length = fotos.length;
+  const remain = length - 4;
+
+  return (
+    <div key={props.id} className="divisi">
+      <div className="_divisi">
+        {!props.isOddChild && <img className="foto-co" src={props.fotoCO} />}
+        <div className={`info info-is-${props.isOddChild}`}>
+          <div className="deskripsi-div">
+            <p className="nama-divisi">{props.divisi}</p>
+            <p className="deskripsi-divisi">{props.deskripsi}</p>
+            {isOpen && <p className="deskripsi-divisi">CO: {props.namaCO}</p>}
+          </div>
+          <div className="pp-anggota">
+            {!isOpen &&
+              fotos
+                .slice(0, 4)
+                .map((foto, key) => (
+                  <img
+                    className="pp"
+                    src={foto}
+                    key={key}
+                    alt={`Staff-${key}`}
+                  />
+                ))}
+
+            {remain > 0 && (
+              <div className={`remain-open-${isOpen}`} onClick={toggle}>
+                {!isOpen ? "+" + remain : <i className="bi bi-x"></i>}
+              </div>
+            )}
+          </div>
+        </div>
+        {props.isOddChild && <img className="foto-co" src={props.fotoCO} />}
+      </div>
+
+      {isOpen && (
+        <div className="all-staff">
+          {fotos.map((foto, key) => (
+            <img className="pp" src={foto} key={key} alt={`Staff-${key}`} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
