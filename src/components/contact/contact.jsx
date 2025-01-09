f4884f9c2fd6b43a59ab2cc65c90bd02758a44ab
kaila asus <mochzhafif1123@student.ub.ac.id>
2025-01-09 11:52:48 +0700

    added popover on contact button

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,18 +1,40 @@
 import "./contact.css";
+import { useState } from "react";
 import telephone from "../../assets/telephone.svg";
 
 export default function ContactButton() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  setTimeout(() => {
+    setIsOpen(false);
+  }, 5000);
+
   return (
-    <a
-      href="https://wa.me/6282247433645"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <div className="kontak-container">
-        <div className="float-button">
-          <img src={telephone} />
-        </div>
+    <div className="main-container">
+      <div
+        className={`popover ${isOpen ? "opened" : "closed"}`}
+        onTransitionEnd={() => handleClose}
+      >
+        <span onClick={handleClose} className="close">
+          <p className="x">&times;</p>
+        </span>
+        <p className="CTA">Hubung kami di sini!</p>
       </div>
-    </a>
+      <a
+        href="https://wa.me/6282247433645"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="kontak-container">
+          <div className="float-button">
+            <img src={telephone} />
+          </div>
+        </div>
+      </a>
+    </div>
   );
 }
